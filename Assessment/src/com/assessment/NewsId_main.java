fa81e42c9dc0a495ee2ab7d17aad0ef135fb7969
vishalpatil937 <vishalpatil937@gmail.com>
2018-09-03 16:47:23 +0530

    updated

--- Assessment/src/com/assessment/NewsId_main.java
+++ Assessment/src/com/assessment/NewsId_main.java
@@ -25,13 +25,13 @@ public class NewsId_main {
 		news_list.add(new News(4, "Def", "User6", "comment4"));
 		news_list.add(new News(8, "Abc", "User7", "comment6"));
 		
-		List<Integer> news_id= news_list.stream().map(News::getNewsId).distinct().collect(toList());
 		
 //		Map<Integer ,Integer> values=news_list.stream()
 //										.collect(toMap(news_list.stream().map(News::getNewsId).distinct(),(Integer)news_list.stream()
 //												.collect(groupingBy(News::getNewsId)).get(news_list.stream().map(News::getNewsId).distinct()).size()));
 		
 //		NewsID with max comments
+		List<Integer> news_id= news_list.stream().map(News::getNewsId).distinct().collect(toList());
 		Map<Integer,List<News>> map=news_list.stream()
 				.collect(groupingBy(News::getNewsId));
 		Map<Integer , Integer> key_value = new HashMap<>();
@@ -40,5 +40,24 @@ public class NewsId_main {
 		}
 		System.out.println("Maximum comments are of News Id:"+key_value.entrySet().stream().max((e1,e2)->e1.getValue()>e2.getValue()?1:-1).get().getKey());
 		
+//		Number of times word "budget" has appeared in the comments
+		
+		System.out.println(news_list.stream()
+			.filter(p->p.getComment().contains("budget"))
+			.collect(toList())
+			.size());
+		
+//		User who has posted maximum comments
+		List<String> users= news_list.stream().map(News::getPostedByUser).distinct().collect(toList());
+		Map<String,List<News>> map_user=news_list.stream()
+				.collect(groupingBy(News::getPostedByUser));
+		
+		Map<String , Integer> user_count = new HashMap<>();
+		for(String i:users) {
+			user_count.put(i, map_user.get(i).size());
+		}
+		System.out.println("Maximum comments are of News Id:"+user_count.entrySet().stream().max((e1,e2)->e1.getValue()>e2.getValue()?1:-1).get().getKey());
+		
+//		Display commentByUser wise number of comments
 	}
 }	
